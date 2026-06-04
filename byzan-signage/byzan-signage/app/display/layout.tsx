6028884ff436b0ec8d68bc35eddfc0df52a6ab8f
bulbule <143988765+bulbule@users.noreply.github.com>
2026-06-04 15:04:43 +0700

    Delete byzan-signage/byzan-signage directory

--- byzan-signage/byzan-signage/app/display/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function DisplayLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="id">
-      <body style={{ margin: 0, padding: 0, background: '#000', overflow: 'hidden' }}>
-        {children}
-      </body>
-    </html>
-  )
-}
